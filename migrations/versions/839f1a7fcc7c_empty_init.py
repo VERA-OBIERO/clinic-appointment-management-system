01cab37bdd1835eaebb5d1824b911e241ed8cbfa
VERA-OBIERO <vera.obiero@gmail.com>
2023-12-14 10:41:37 +0300

    create database with the models as tables

---
+++ migrations/versions/839f1a7fcc7c_empty_init.py
@@ -0,0 +1,24 @@
+"""Empty Init
+
+Revision ID: 839f1a7fcc7c
+Revises: 
+Create Date: 2023-12-14 10:27:58.494073
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '839f1a7fcc7c'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass

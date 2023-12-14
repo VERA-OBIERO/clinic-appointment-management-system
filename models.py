9618d27f48c79a2d9438f0bf948da38c01f0cddd
VERA-OBIERO <vera.obiero@gmail.com>
2023-12-14 09:51:22 +0300

    define appointment model and add relationships

--- models.py
+++ models.py
@@ -1,5 +1,6 @@
 from sqlalchemy import create_engine 
-from sqlalchemy import Column, Integer, String, Boolean
+from sqlalchemy import Column, Integer, String, Boolean, ForeignKey, DateTime
+from sqlalchemy.orm import relationship
 from sqlalchemy.ext.declarative import declarative_base 
 
 engine = create_engine('sqlite:///clinic_appointment_manager.db')
@@ -19,6 +20,8 @@ class Doctor(Base):
     phone = Column(String(15))
     is_available = Column(Boolean, default=True)
 
+    appointments = relationship("Appointment", back_populates="doctor")
+
     def __repr__(self):
         return f"<Dr(name='{self.last_name}', specialty='{self.specialty}')>"
 
@@ -31,8 +34,22 @@ class Patient(Base):
     age = Column(Integer)
     contact = Column(String(25))
 
+    appointments = relationship("Appointment", back_populates="patient")
+
     def __repr__(self):
         return f"<Patient(name='{self.last_name}')>"
     
-    
 
+class Appointment(Base):
+    __tablename__ = 'appointments'
+
+    id = Column(Integer, primary_key=True)
+    doctor_id = Column(Integer, ForeignKey('doctors.id'), nullable=False)
+    patient_id = Column(Integer, ForeignKey('patients.id'), nullable=False)
+    appointment_time = Column(DateTime, nullable=False)
+
+    doctor = relationship("Doctor", back_populates="appointments")
+    patient = relationship("Patient", back_populates="appointments")
+
+    def __repr__(self):
+        return f"<Appointment(doctor='{self.doctor_id}', patient='{self.patient_id}', time='{self.appointment_time}')>"

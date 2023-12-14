e247f0e829e48e1eebe1424b8ca47c20eb375ec1
VERA-OBIERO <vera.obiero@gmail.com>
2023-12-14 22:12:21 +0300

    add comments

--- app.py
+++ app.py
@@ -10,22 +10,22 @@ Base.metadata.bind = engine
 Session = sessionmaker(bind=engine)
 session = Session()
 
-@click.group()
+@click.group() # Define a Click command group
 def cli():
     pass 
 
-@cli.command()
+@cli.command() # Command to create a patient
 @click.option('--first-name', prompt='First Name', help='First name of the patient')
 @click.option('--last-name', prompt='Last Name', help='Last name of the patient')
 @click.option('--age', prompt='Age', type=int, help='Age of the patient')
 @click.option('--contact', prompt='Contact', help='Contact information of the patient')
-def create_patient(first_name, last_name, age, contact):
+def create_patient(first_name, last_name, age, contact): # Create a Patient object and add it to the session
     patient = Patient(first_name=first_name, last_name=last_name, age=age, contact=contact)
     session.add(patient)
     session.commit()
     click.echo('Patient created successfully!') 
 
-@cli.command()
+@cli.command() #Command to create a doctor
 @click.option('--first-name', prompt='First Name', help='First name of the doctor')
 @click.option('--last-name', prompt='Last Name', help='Last name of the doctor')
 @click.option('--gender', prompt='Gender', help='Gender of the doctor')
@@ -33,14 +33,14 @@ def create_patient(first_name, last_name, age, contact):
 @click.option('--email', prompt='Email', help='Email of the doctor')
 @click.option('--phone', prompt='Phone', help='Phone number of the doctor')
 @click.option('--availability', prompt='Availability', type=bool, help='Doctor availability')
-def create_doctor(first_name, last_name, gender, specialty, email, phone, availability):
+def create_doctor(first_name, last_name, gender, specialty, email, phone, availability): # Create a Doctor object and add it to the session
     doctor = Doctor(first_name=first_name, last_name=last_name, gender=gender, specialty=specialty,
                     email=email, phone=phone, is_available=availability)
     session.add(doctor)
     session.commit()
     click.echo('Doctor created successfully!')
 
-@cli.command()
+@cli.command() #Command to list patients
 def list_patients():
     patients = session.query(Patient).all()
 
@@ -51,7 +51,7 @@ def list_patients():
     else:
         click.echo("No patients available.")
 
-@cli.command()
+@cli.command() #Command to list doctors
 def list_doctors():
     doctors = session.query(Doctor).all()
 
@@ -62,7 +62,7 @@ def list_doctors():
     else:
         click.echo("No doctors available.")
 
-@cli.command()
+@cli.command() #Command to make an appointment
 @click.option('--doctor-id', prompt='Doctor ID', type=int, help='ID of the doctor')
 @click.option('--patient-id', prompt='Patient ID', type=int, help='ID of the patient')
 @click.option('--appointment-time', prompt='Appointment Time', help='Date and time of the appointment (YYYY-MM-DD HH:MM)')
@@ -84,7 +84,7 @@ def make_appointment(doctor_id, patient_id, appointment_time):
     else:
         click.echo('Doctor or Patient ID not found. Please check and try again.')
 
-# Define Merge Sort algorithm
+# Define Merge Sort algorithm to sort appointments by date and time
 def merge_sort(appointments):
     if len(appointments) <= 1:
         return appointments
@@ -114,7 +114,7 @@ def merge(left, right):
     result.extend(right[right_index:])
     return result
 
-@cli.command()
+@cli.command() #Command to list appointments
 def list_appointments():
     appointments = session.query(Appointment).all()
 
@@ -128,7 +128,7 @@ def list_appointments():
     else:
         click.echo("No appointments available.")
 
-@cli.command()
+@cli.command() #Command to edit appointment time
 @click.option('--appointment-id', prompt='Appointment ID', type=int, help='ID of the appointment to update')
 @click.option('--new-appointment-time', prompt='New Appointment Time', help='New date and time for the appointment (YYYY-MM-DD HH:MM)')
 def update_appointment(appointment_id, new_appointment_time):
@@ -146,9 +146,9 @@ def update_appointment(appointment_id, new_appointment_time):
     else:
         click.echo('Appointment ID not found. Update failed.') 
 
-@cli.command()
+@cli.command() # Command to delete an appointment
 @click.option('--appointment-id', prompt='Appointment ID', type=int, help='ID of the appointment to delete')
-def delete_appointment(appointment_id):
+def delete_appointment(appointment_id): # Deleting an appointment by ID
     appointment = session.query(Appointment).get(appointment_id)
     if appointment:
         session.delete(appointment)
@@ -157,6 +157,6 @@ def delete_appointment(appointment_id):
     else:
         click.echo('Appointment ID not found. Deletion failed.')
 
-
+# Execute the command line interface if the script is run directly
 if __name__ == '__main__':
     cli()
